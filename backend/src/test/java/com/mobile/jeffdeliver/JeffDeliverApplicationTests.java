a9e64c88346442abb304073a8659e77a6a3af1d9
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-01-07 20:41:15 -0300

    criação do projeto

---
+++ backend/src/test/java/com/mobile/jeffdeliver/JeffDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.jeffdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeffDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
