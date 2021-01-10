f31c7b15d34afcfac240ca362ea52c33205af8ff
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-01-10 19:54:46 -0300

    estutura inicial web

---
+++ front-web/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+
+function Navbar() {
+    return(
+        <nav className='main-navbar' >
+            <a href='home'>
+                
+            </a>
+            <a href='home' className='logo-text' >
+                JeffDeliver
+            </a>
+        </nav>
+    )
+}
