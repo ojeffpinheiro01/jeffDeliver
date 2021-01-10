6dd07703fd52d0acb57db55a5b7a14d60180c3ea
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-01-10 20:40:38 -0300

    add home

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar'
+import Home from './Components/Home'
 
 function App() {
-  return <Navbar /> 
+  return(
+    <>
+      <Navbar />
+      <Home />
+    </>
+  )
 }
 
 export default App;
