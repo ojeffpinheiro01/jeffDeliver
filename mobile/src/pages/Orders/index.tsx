2b127cdce121b337bd5804cf289cc8107ec0f814
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-01-13 00:26:00 -0300

    interação com o db

--- mobile/src/pages/Orders/index.tsx
+++ mobile/src/pages/Orders/index.tsx
@@ -1,24 +1,37 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, ScrollView, View, Alert, Text } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { fetchOrders } from '../../api';
 import Header from '../../components/Header';
 import OrderCard from '../../components/OrderCard';
+import { Order } from '../../types';
 
 function Orders() {
+    const [orders, setOrders] = useState<Order[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigation = useNavigation();
+    const isFocused = useIsFocused();
     useEffect(() => {
+        setIsLoading(true);
         fetchOrders()
-            .then(response => console.log(response.data))
-            .catch(error => console.log(error));
+            .then(response => setOrders(response.data))
+            .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
+            .finally(() => setIsLoading(false));
     }, []);
     return (
         <>
             <Header />
             <ScrollView style={styles.container} >
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
-                <OrderCard />
+                {isLoading ? (
+                    <Text>Buscando pedidos</Text>
+                ) : (
+                    orders.map(order => (
+                        <TouchableWithoutFeedback key={order.id} >
+                            <OrderCard order={order} />
+                        </TouchableWithoutFeedback>
+                    ))
+                )}
             </ScrollView>
         </>
     );
