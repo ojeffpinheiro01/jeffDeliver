9dd398bef08ea44c645c2c1842f65e624cc789bb
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-01-13 00:05:41 -0300

    corrigindo host

--- mobile/src/pages/Orders/index.tsx
+++ mobile/src/pages/Orders/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
+import { fetchOrders } from '../../api';
 import Header from '../../components/Header';
 import OrderCard from '../../components/OrderCard';
 
 function Orders() {
-
+    useEffect(() => {
+        fetchOrders()
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error));
+    }, []);
     return (
         <>
             <Header />
