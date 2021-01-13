a4ebf43132f813ec0159ce151bd67329b5c58a32
ojeffpinheiro <jefersonpinheirodesouza@gmail.com>
2021-01-13 00:44:26 -0300

    formatando data

--- mobile/src/components/OrderCard/index.tsx
+++ mobile/src/components/OrderCard/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import dayjs from 'dayjs'
+import 'dayjs/locale/pt-br'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { Order } from '../../types';
+
+dayjs.locale('pt-br');
+dayjs.extend(relativeTime);
 
 type Props = {
   order: Order;
+}
+
+function dateFromNow(date: string) {
+  return dayjs(date).fromNow();
 }
 
 function OrderCard({ order }: Props) {
@@ -14,7 +24,7 @@ function OrderCard({ order }: Props) {
                 <Text style={styles.orderName}>Pedido {order.id}</Text>
                 <Text style={styles.orderPrice}>R$ {order.total}</Text>
             </View>
-            <Text style={styles.text}>{order.moment}</Text>
+            <Text style={styles.text}>{dateFromNow(order.moment)}</Text>
             <View style={styles.productsList}>
               {order.products.map(product => (
                 <Text key={product.id} style={styles.text}>{product.name}</Text>
